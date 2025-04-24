856595d4588c234ac64a960b853af3bf41cbc950
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-04-24 16:48:15 +0000

    php

--- answer.php
+++ answer.php
@@ -33,12 +33,12 @@
   <title>Random Number Guess, PHP</title>
 </head>
 
-<body>
+<<body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Random Number Guess, PHP</span>
+        <span class="mdl-layout-title">Random Number Guess, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
@@ -46,30 +46,29 @@
         <img src="./images/dice.gif" />
       </div>
       <div class="page-content-php">
-        <div id="check">
+        <div id="user-info">
           <?php
-          $randomnumber = rand(1, 6);
+          $randomNumber = random_int(1, 6);
           //input
-          $usernumber = $_GET["number"];
+          $userNumber = (int) $_GET["userNumber"];
           // process
-          if ($usernumber === $randomnumber) {
-            $randomnumber = rand(1, 6);
-            //output 
-            echo 'You have guessed the correct number!';
+          if ($userNumber === $randomNumber) {
+            // output
+            echo "The number " . $userNumber . " was the correct number";
           }
-          if ($usernumber !== $randomnumber) {
-          // output 
-          echo "you have guessed the wrong number!" + 
-          "<br/>The Correct Number Was:". $randomnumber .
+          // process
+          if ($userNumber === $randomNumber) {
+            echo "You have guessed the correct number!";
+          } else {
+            echo "You have guessed the wrong number!<br/>The Correct Number Was: " . $randomNumber;
           }
           ?>
+          <div class="page-content-answer">
+            <a href="./index.php">Return</a>
+          </div>
         </div>
-        <div class="page-content-answer">
-          <a href="./index.php">Return ...</a>
-        </div>
-      </div>
     </main>
   </div>
-</body>
+  </body>
 
 </html>
--- index.php
+++ index.php
@@ -36,31 +36,32 @@
 
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <script src="./js/script.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Random Number Guess, PHP</span>
+        <span class="mdl-layout-title">Random Number Guess, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/dice.gif" />
+        <img src="./images/dice.gif" alt="dice gif" />
       </div>
+      <div class="page-content">Pick a number between 1 and 6</div>
+      <br />
       <br />
       <div class="page-content-php">
-        <form action="answer.php" method="GET">
-          <p>Number</p>
+        <form action="./answer.php" method="GET">
           <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="number">
-            <label class="mdl-textfield__label" for="number">Enter Number...</label>
+            <input class="mdl-textfield__input" type="number" name="userNumber" />
+            <label class="mdl-textfield__label" for="userNumber">enter</label>
             <span class="mdl-textfield__error">Input is not a number!</span>
           </div>
-          <br />
           <br />
           <!-- Accent-colored raised button with ripple -->
           <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
             type="submit">
-            Check
+            Check!
           </button>
         </form>
       </div>
